501e794bf51cec8ba84733a7f16b6d0491014e8e
fernandooarroyoo <arroyocarmonaf@gmail.com>
2025-09-24 10:31:47 +0200

    Clase Shield

--- src/irrgarten/Irrgarten.java
+++ src/irrgarten/Irrgarten.java
@@ -1,21 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package irrgarten;
 
-/**
- *
- * @author fernando
- */
 public class Irrgarten {
 
-    /**
-     * @param args the command line arguments
-     */
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       Directions direccion = Directions.DOWN;
+               
     }
     
 }
