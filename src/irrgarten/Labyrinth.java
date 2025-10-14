95da0f1979df4e99b327973d0a1e839a7a551f06
fernandooarroyoo <arroyocarmonaf@gmail.com>
2025-10-14 17:55:47 +0200

    PRACTICA 2 DIAGRAMA COPIADO

--- src/irrgarten/Labyrinth.java
+++ src/irrgarten/Labyrinth.java
@@ -14,4 +14,79 @@ public class Labyrinth {
     private int exitRow;
     private int exitCol;
     
+    public Labyrinth(int nRows, int nCols, int exitRow, int exitCol){
+        throw new UnsupportedOperationException();
+    }
+    
+    public void spreadPlayers(Player players[]){
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean haveAWinner(){
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void addMonster(int row, int col, Monster monster){
+        throw new UnsupportedOperationException();
+    }
+    
+    public Monster putPlayer(Directions direction, Player player){
+        throw new UnsupportedOperationException();
+    }
+    
+    public void addBlock(Orientation orientation, int startRow, int startCol, int length){
+        throw new UnsupportedOperationException();
+    }
+    
+    public Directions[] validMoves(int row, int col){
+        throw new UnsupportedOperationException();
+    }
+    
+    private boolean posOK(int row, int col){
+        throw new UnsupportedOperationException();
+    }
+    
+    private boolean emptyPos(int row, int col){
+        throw new UnsupportedOperationException();
+    }
+    
+    private boolean combatPos(int row, int col){
+        throw new UnsupportedOperationException();
+    }
+    
+    private boolean canStepOn(int row, int col){
+        throw new UnsupportedOperationException();
+    }
+    
+    private void updateOldPos(int row, int col){
+        throw new UnsupportedOperationException();
+    }
+    
+    private int[] dir2Pos(int row, int col, Directions direction){
+        throw new UnsupportedOperationException();
+    }
+    
+    private int[] randomEmptyPos(){
+        throw new UnsupportedOperationException();
+    }
+    
+    private Monster putPlayer2D(int oldRow, int oldCol, int row, int col, Player player){
+        throw new UnsupportedOperationException();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
